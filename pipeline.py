3094a90c38ec1fa390295e458df4c9f4c168cb92
Pavani-Pulanagari <pavani1231112@gmail.com>
2026-05-01 15:01:39 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,1 +1,17 @@
+from text_generation import generate_text
+from translation import translate_text
+from rag_pipeline import ask_question
 
+def run_pipeline(task, input_text):
+    
+    if task == "generate":
+        return generate_text(input_text)
+    
+    elif task == "translate":
+        return translate_text(input_text)
+    
+    elif task == "qa":
+        return ask_question(input_text)
+    
+    else:
+        return "Invalid task. Use: generate / translate / qa"
