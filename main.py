d5605aa907e229727dcd9d755c276359f98e1fe4
Pavani-Pulanagari <pavani1231112@gmail.com>
2026-05-01 15:03:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from pipeline import run_pipeline
 
+if __name__ == "__main__":
+    
+    print("Select Task: generate / translate / qa")
+    task = input("Enter task: ")
+
+    user_input = input("Enter your input: ")
+
+    result = run_pipeline(task, user_input)
+
+    print("\nOutput:")
+    print(result)
