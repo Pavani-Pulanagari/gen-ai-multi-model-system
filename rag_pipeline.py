b790dac75ddeaa1c0ed7a144d088aad55f34c8e3
Pavani-Pulanagari <pavani1231112@gmail.com>
2026-05-01 15:53:34 -0400

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,6 +1,6 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from transformers import pipeline
 
