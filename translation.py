a6329d5a8253bd4b8f49d0174595d0cb3a1b533d
Pavani-Pulanagari <pavani1231112@gmail.com>
2026-05-01 14:58:03 -0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,1 +1,16 @@
+from transformers import MarianMTModel, MarianTokenizer
+import torch
 
+def translate_text(text):
+    model_name = "Helsinki-NLP/opus-mt-en-fr"
+
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+
+    tokens = tokenizer(text, return_tensors="pt", padding=True).to(device)
+    translated = model.generate(**tokens)
+
+    return tokenizer.decode(translated[0], skip_special_tokens=True)
